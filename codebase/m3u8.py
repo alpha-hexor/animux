a7c6f4c10149af3a79e1baa42109086d6dcc676a
unknown <itsalexwilliams69@gmail.com>
2022-04-04 08:02:11 -0700

    m3u8 parsing fixed

--- codebase/m3u8.py
+++ codebase/m3u8.py
@@ -2,9 +2,12 @@
 python code to parse m3u8 quality
 '''
 
-
-from statistics import quantiles
 import requests
+import regex
+
+#regex 
+start_regex = regex.compile(r"#EXT-X-STREAM-INF(:.*?)?\n+(.+)")
+res_regex = regex.compile(r"RESOLUTION=\d+x(\d+)")
 
 headers={
     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
@@ -14,25 +17,22 @@ headers={
 
 def get_m3u8_quality(link):
     
-    
-    partial_link = link[:link.rfind('/')+1]
-    
-    
-    
-    
     links = []
     qualities = []
     
-    r=requests.get(link,headers=headers)
-    s=r.text.replace("#EXTM3U\n","").strip()
-    s=s.split("\n")
+    partial_link = link[:link.rfind('/')+1]
     
-    for i in range(len(s)):
-        if i%2 == 1:
-            links.append(partial_link+s[i])
-        else:
-            q=s[i].split(",")[-1].replace("NAME=","").replace('"','')
-            qualities.append(q)
+    r=requests.get(link,headers=headers)
+    
+    
+    for i in start_regex.finditer(r.text):
+        res_line,l = i.groups()
+        
+        #construct the quality 
+        qualities.append(str(res_regex.search(res_line).group(1))+"p")
+        
+        #construct link
+        links.append(partial_link+l.strip())
     
     return qualities,links
 
